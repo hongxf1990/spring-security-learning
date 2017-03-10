9339ef2b6e14b383eba16facbd3b480ad3510d66
hongxf1990 <hongxf19900420@gmail.com>
2017-03-10 13:02:27 +0800

    基于XML配置自定义登录页面

--- hello-world/src/main/java/com/petter/web/HelloController.java
+++ hello-world/src/main/java/com/petter/web/HelloController.java
@@ -3,6 +3,7 @@ package com.petter.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -20,7 +21,6 @@ public class HelloController {
         model.addObject("message", "This is welcome page !");
         model.setViewName("hello");
         return model;
-
     }
 
     @RequestMapping(value = "/admin**", method = RequestMethod.GET)
@@ -30,8 +30,24 @@ public class HelloController {
         model.addObject("title", "Spring Security Hello World");
         model.addObject("message", "This is protected page!");
         model.setViewName("admin");
-
         return model;
-
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "不正确的用户名和密码");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "你已经成功退出");
+        }
+        model.setViewName("login");
+        return model;
+    }
+
 }
